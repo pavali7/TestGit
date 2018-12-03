3d242799e34cf3b600f0b855015e09dfbaa3736c
Paweł Pięta <ppieta@zdz.local>
2018-12-03 10:40:19 +0100

    Zmiany2

---
+++ js/add-content.js
@@ -0,0 +1,13 @@
+var today = new Date();
+var hourNow = today.getHours();
+var greeting;
+
+	if (hourNow > 18) {
+		greeting = "Dobry wieczór!";}
+	else if (hourNow > 12){
+		greeting = "Dzien dobry";} 
+	else if (hourNow > 0){
+		greeing = "dzien dobry";}
+	else {
+		greeting = "Witamy";}
+	document.write('<h3>' + greeting + '</h3'>);
